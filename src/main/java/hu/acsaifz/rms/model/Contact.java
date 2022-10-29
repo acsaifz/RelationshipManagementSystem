f974e7e5ca83b045f3806357339898589e678cd1
acsaifz <angeluszmail@gmail.com>
2022-10-29 18:06:09 +0200

    create Contact

---
+++ src/main/java/hu/acsaifz/rms/model/Contact.java
@@ -0,0 +1,31 @@
+package hu.acsaifz.rms.model;
+
+public class Contact {
+    private long id;
+    private String phone;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
