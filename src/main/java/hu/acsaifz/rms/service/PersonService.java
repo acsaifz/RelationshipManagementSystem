f5d267272885dd33f09460f0696bc5707b2fc95c
acsaifz <angeluszmail@gmail.com>
2022-10-29 19:34:57 +0200

    implement base functions

--- src/main/java/hu/acsaifz/rms/service/PersonService.java
+++ src/main/java/hu/acsaifz/rms/service/PersonService.java
@@ -5,6 +5,8 @@ import hu.acsaifz.rms.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
@@ -16,5 +18,17 @@ public class PersonService {
 
     public void save(Person person){
         personRepository.save(person);
+    }
+
+    public List<Person> findAll(){
+        return (List<Person>) personRepository.findAll();
+    }
+
+    public Person findById(long id){
+        return personRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No such person: id = " + id));
+    }
+
+    public void delete(Person person){
+        personRepository.delete(person);
     }
 }
