6504ce99f30d8ce7ed58783da0f6ae8f2d4edd6b
acsaifz <angeluszmail@gmail.com>
2022-10-29 17:44:07 +0200

    Initial commit

---
+++ src/main/java/hu/acsaifz/rms/RelationshipManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package hu.acsaifz.rms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationshipManagementSystemApplication.class, args);
+    }
+
+}
