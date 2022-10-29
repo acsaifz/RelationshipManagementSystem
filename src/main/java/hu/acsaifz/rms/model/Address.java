cdaf965b158f34cae81427166b102168d68e055e
acsaifz <angeluszmail@gmail.com>
2022-10-29 18:05:17 +0200

    create Address

---
+++ src/main/java/hu/acsaifz/rms/model/Address.java
@@ -0,0 +1,58 @@
+package hu.acsaifz.rms.model;
+
+public class Address {
+    private long id;
+    private String country;
+    private String postalCode;
+    private String city;
+    private String address;
+    private Contact contact;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+}
