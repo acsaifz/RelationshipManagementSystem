02d3a9cede6c5ada089939e2692dcfd24737d4a1
acsaifz <angeluszmail@gmail.com>
2022-10-30 15:43:35 +0100

    add new endpoints

--- src/main/java/hu/acsaifz/rms/controller/RmsController.java
+++ src/main/java/hu/acsaifz/rms/controller/RmsController.java
@@ -1,11 +1,14 @@
 package hu.acsaifz.rms.controller;
 
+import hu.acsaifz.rms.dto.PersonDto;
 import hu.acsaifz.rms.model.Person;
 import hu.acsaifz.rms.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -27,6 +30,21 @@ public class RmsController {
 
     @GetMapping(value = {"/person/add"})
     public String showAddPersonForm(Model model){
+        model.addAttribute("editPerson", false);
+        return "person";
+    }
+
+    @PostMapping(value = {"/person/add"})
+    public String addPerson(PersonDto personDto){
+        Person person = personDto.createPerson();
+        return "redirect:/person/" + person.getId() + "/edit";
+    }
+
+    @GetMapping(value = {"/person/{id}/edit"})
+    public String editPerson(@PathVariable long id, Model model){
+        Person person = personService.findById(id);
+        model.addAttribute("person", person);
+        model.addAttribute("editPerson", true);
         return "person";
     }
 }
