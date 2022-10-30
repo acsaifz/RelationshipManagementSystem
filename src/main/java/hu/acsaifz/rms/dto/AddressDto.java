2dcd5b9749ab56557c1dd36bbdcece0635157c71
acsaifz <angeluszmail@gmail.com>
2022-10-30 14:50:41 +0100

    create AddressDto

---
+++ src/main/java/hu/acsaifz/rms/dto/AddressDto.java
@@ -0,0 +1,63 @@
+package hu.acsaifz.rms.dto;
+
+import hu.acsaifz.rms.model.Address;
+
+public class AddressDto {
+    private long id;
+    private String country;
+    private String postalCode;
+    private String city;
+    private String address;
+
+    public AddressDto() {
+
+    }
+
+    public AddressDto(Address address) {
+        this.id = address.getId();
+        this.country = address.getCountry();
+        this.postalCode = address.getPostalCode();
+        this.city = address.getCity();
+        this.address = address.getAddress();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
