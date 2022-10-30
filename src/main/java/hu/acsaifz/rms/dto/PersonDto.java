3df5f02fb6526f111da05caac6a9100ca10828b4
acsaifz <angeluszmail@gmail.com>
2022-10-30 14:22:44 +0100

    create PersonDto

---
+++ src/main/java/hu/acsaifz/rms/dto/PersonDto.java
@@ -0,0 +1,56 @@
+package hu.acsaifz.rms.dto;
+
+import hu.acsaifz.rms.model.Person;
+
+import java.time.LocalDate;
+
+public class PersonDto {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+
+
+    public PersonDto() {
+
+    }
+
+    public PersonDto(Person person) {
+        this.id = person.getId();
+        this.firstName = person.getFirstName();
+        this.lastName = person.getLastName();
+        this.birthDate = person.getBirthDate();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
