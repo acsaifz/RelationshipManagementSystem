0cca0ef83e011ba4f7051b4d92708511db36e422
acsaifz <angeluszmail@gmail.com>
2022-10-30 08:18:39 +0100

    create RmsController

---
+++ src/main/java/hu/acsaifz/rms/controller/RmsController.java
@@ -0,0 +1,27 @@
+package hu.acsaifz.rms.controller;
+
+import hu.acsaifz.rms.model.Person;
+import hu.acsaifz.rms.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class RmsController {
+    private PersonService personService;
+
+    @Autowired
+    public void setPersonService(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping(value = {"/"})
+    public String showPersons(Model model){
+        List<Person> persons = personService.findAll();
+        model.addAttribute("persons", persons);
+        return "index";
+    }
+}
