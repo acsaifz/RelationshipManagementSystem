ffb2999f51989a2c5dc9d3735784bea418f8de22
acsaifz <angeluszmail@gmail.com>
2022-10-31 10:56:50 +0100

    add save function

--- src/main/java/hu/acsaifz/rms/service/ContactService.java
+++ src/main/java/hu/acsaifz/rms/service/ContactService.java
@@ -1,5 +1,7 @@
 package hu.acsaifz.rms.service;
 
+import hu.acsaifz.rms.dto.ContactDto;
+import hu.acsaifz.rms.model.Address;
 import hu.acsaifz.rms.model.Contact;
 import hu.acsaifz.rms.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ContactService {
     private ContactRepository contactRepository;
+    private AddressService addressService;
+
+    @Autowired
+    public void setAddressService(AddressService addressService) {
+        this.addressService = addressService;
+    }
 
     @Autowired
     public void setContactRepository(ContactRepository contactRepository) {
@@ -16,5 +24,10 @@ public class ContactService {
 
     public void delete(Contact contact){
         contactRepository.delete(contact);
+    }
+
+    public Contact save(ContactDto contactDto, long addressId){
+        Address address = addressService.findById(addressId);
+        return contactRepository.save(contactDto.createContact(address));
     }
 }
