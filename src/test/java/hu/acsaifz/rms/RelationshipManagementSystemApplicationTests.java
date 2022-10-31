a42bc4dd6f993c1ea6adc27997f6b0d75495394f
acsaifz <angeluszmail@gmail.com>
2022-10-31 16:34:22 +0100

    fix test

--- src/test/java/hu/acsaifz/rms/RelationshipManagementSystemApplicationTests.java
+++ src/test/java/hu/acsaifz/rms/RelationshipManagementSystemApplicationTests.java
@@ -3,7 +3,7 @@ package hu.acsaifz.rms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RelationshipManagementSystemApplicationTests.class)
 class RelationshipManagementSystemApplicationTests {
 
     @Test
